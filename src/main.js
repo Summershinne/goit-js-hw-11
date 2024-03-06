82163ac702c7fafff9caa0b336b55f5568acfc1b
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-03-06 22:29:16 +0100

    hw

--- src/main.js
+++ src/main.js
@@ -13,24 +13,24 @@ const input = document.querySelector('[name="searchQuery"]');
 const load = document.querySelector('.load');
 let searchQuery = "";
 form.addEventListener("submit", onSubmit);
-
+const lightbox =
+    new SimpleLightbox('.gallery a', {
+        nav: true,
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
 function onSubmit(event) {
     event.preventDefault();
-    load.classList.add('visually-hidden');
+    load.classList.remove('visually-hidden');
     container.innerHTML = "";
     searchQuery = input.value.trim();
    
     getImages(searchQuery)
         .then(images => {
-        load.classList.remove('visually-hidden');
+        load.classList.add('visually-hidden');
             container.insertAdjacentHTML("beforeend", render(images)); 
-            const lightbox =
-                new SimpleLightbox('.gallery a', {
-                nav: true,
-                captions: true,
-                captionsData: 'alt',
-                captionDelay: 250,
-                })
+            
             lightbox.refresh();
             
         })
